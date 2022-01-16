342aeb2c04680916037cd17c00e3ca9b7cfcb065
Alex <cazac9@gmail.com>
2022-01-16 15:59:42 +0200

    added actual pins

--- src/main.cpp
+++ src/main.cpp
@@ -5,78 +5,82 @@
 #include <max6675.h>
 #include <Ticker.h>
 
+#define SDA_PIN 22
+#define SCL_PIN 23
+
 byte currentTemp;
 byte targetTemp;
 byte power;
-bool isCooling;
+// bool isCooling;
 
 #define MAX_POWER  3
 #define MIN_POWER  1
-#define PIN_4KWT  1
-#define PIN_8KWT  2
-#define PIN_12KWT  3
-byte powerPins [3]= {PIN_4KWT, PIN_8KWT, PIN_12KWT};
+// #define PIN_4KWT  1
+// #define PIN_8KWT  2
+// #define PIN_12KWT  3
+// byte powerPins [3]= {PIN_4KWT, PIN_8KWT, PIN_12KWT};
 
 
 // screen properties
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3D 
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
 #define SCREEN_WIDTH   128
 #define SCREEN_HEIGHT  64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// rotary encoder properties
-#define ROTARY_PIN1 1//D1
-#define ROTARY_PIN2 2 //D2
-#define BUTTON_PIN  4 //D4
-#define CLICKS_PER_STEP 4   // this number depends on your rotary encoder
-#define MIN_POS         20
-#define MAX_POS         80
-#define START_POS       20
-#define INCREMENT       3   // this number is the counter increment on each step
-ESPRotary r;
-Button2 b;
-
-// termopair properties
-int thermoDO = 4;
-int thermoCS = 5;
-int thermoCLK = 6;
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
-
-// timer
-Ticker timer(stopCoolingOff, 3*60*1000, 0, MICROS_MICROS);
-
-void setup() {
-  // pin modes
-  pinMode(13, OUTPUT); 
-
-  // init serial
-  Serial.begin (9600);
-  delay(50);
-  
-  setDefaultParams(b);
-
-  initRotaryEncoder();
-
-  initDisplay();
-}
-
 void initDisplay(){
+  Serial.println(F("Start initing display"));
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
   display.display();
   delay(2000);
-
-  // Clear the buffer
   display.clearDisplay();
 }
 
+void writeTextOnScreen(){
+  display.clearDisplay();
+  display.setTextSize(1);             
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);             
+  display.printf("C: %i; T: %i; P: %i", currentTemp, targetTemp, power);
+  display.display();
+}
+
+// rotary encoder properties
+#define ROTARY_PIN1 12
+#define ROTARY_PIN2 13 
+#define BUTTON_PIN  14 
+#define CLICKS_PER_STEP 4   
+#define MIN_POS         20
+#define MAX_POS         80
+#define START_POS       20
+#define INCREMENT       3  
+ESPRotary r;
+Button2 b;
+
+void setPower(Button2& btn){
+  Serial.println("Pressed set power");
+  power++;
+  if(power > MAX_POWER)
+    power = MIN_POWER;
+}
+
+void setDefaultParams(Button2& btn){
+  Serial.println("Pressed set default params");
+  power = 2;
+  targetTemp = 40;
+}
+
+void manageTemperatureChange(ESPRotary& r) {
+  Serial.println("Changed target temperature value");
+  targetTemp = r.getPosition();
+}
+
 void initRotaryEncoder(){
+  Serial.println("Start initing rotary encoder");
   b.begin(BUTTON_PIN);
   b.setTapHandler(setPower);
   b.setLongClickHandler(setDefaultParams);
@@ -84,8 +88,31 @@ void initRotaryEncoder(){
   r.setChangedHandler(manageTemperatureChange);
 }
 
+// termopair properties
+#define TERMO_SCLK 32
+#define TERMO_CS 33
+#define TERMO_SO 25
+MAX6675 thermocouple(TERMO_SCLK, TERMO_CS, TERMO_SO);
+
+// // timer
+// Ticker timer(stopCoolingOff, 3*60*1000, 0, MICROS_MICROS);
+
+
+void setup() {
+  Serial.begin (115200);
+  Wire.begin(SDA_PIN, SCL_PIN);
+
+  initDisplay();
+  initRotaryEncoder();
+  setDefaultParams(b);
+}
+
+
+
+
+
 void loop() {
-  timer.update();
+  // timer.update();
 
   // read target temperature
   r.loop();
@@ -93,56 +120,32 @@ void loop() {
 
   // read current temperature
   currentTemp = (byte)thermocouple.readCelsius();
+  Serial.println(thermocouple.readCelsius());
+  delay(500);
 
-  if(!isCooling){
-    for (byte i = 0; i < power; i++)
-    {
-      byte powerPin = powerPins[i];
-      if (currentTemp < targetTemp){
-        // start heating
-        digitalWrite(powerPin, HIGH);
-      }
-      else{
-        // wait for coolling
-        isCooling = true;
-        timer.start();
-        digitalWrite(powerPin, LOW);
-      }
-    }
-  }
+  // if(!isCooling){
+  //   for (byte i = 0; i < power; i++)
+  //   {
+  //     byte powerPin = powerPins[i];
+  //     if (currentTemp < targetTemp){
+  //       // start heating
+  //       digitalWrite(powerPin, HIGH);
+  //     }
+  //     else{
+  //       // wait for coolling
+  //       isCooling = true;
+  //       timer.start();
+  //       digitalWrite(powerPin, LOW);
+  //     }
+  //   }
+  // }
   
 
   writeTextOnScreen();
-  delay(500);
+  // delay(500);
 }
 
-void stopCoolingOff(){
- isCooling = false;
- timer.stop();
-}
-
-void writeTextOnScreen(){
-  display.clearDisplay();
-
-  display.setTextSize(1);             
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);             
-  display.printf(PSTR("C: %i; T: %i; P: %i"), currentTemp, targetTemp, power);
-
-  display.display();
-}
-
-void setPower(Button2& btn){
-  power++;
-  if(power > MAX_POWER)
-    power = MIN_POWER;
-}
-
-void setDefaultParams(Button2& btn){
-  power = 2;
-  targetTemp = 40;
-}
-
-void manageTemperatureChange(ESPRotary& r) {
-  targetTemp = r.getPosition();
-}
+// void stopCoolingOff(){
+//  isCooling = false;
+//  timer.stop();
+// }
