4e399ebd8f48e1caede01609d016e9129e81522a
ohumeniuk <ohumeniuk@crownresorts.com.au>
2022-01-20 16:47:57 +0200

    time#2

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <Globals.h>
 #include <WifiMonitor.h>
 #include <TimeManager.h>
+#include <esp_sntp.h>
+#include <time.h>
+
 //todo:
 // do something with innertion
 // http
@@ -62,6 +65,22 @@ void createTask(TaskFunction_t task, const char * name, QueueHandle_t q, TaskHan
 }
 
 void setup() {
+  sntp_setoperatingmode(SNTP_OPMODE_POLL);
+  sntp_setservername(0, "pool.ntp.org");
+  sntp_init();
+
+  time_t now;
+  char strftime_buf[64];
+  struct tm timeinfo;
+
+  time(&now);
+  setenv("TZ", "CST-8", 1);
+  tzset();
+
+localtime_r(&now, &timeinfo);
+strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
+ESP_LOGI(TAG, "The current date/time in Shanghai is: %s", strftime_buf);
+
   Serial.begin (115200);
   inputQ = createQueue("input");
   displayQ = createQueue("display");
