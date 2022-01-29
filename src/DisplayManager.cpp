4cc1bddbc7d88255228632f23c1f2572deaf285d
Alex <cazac9@gmail.com>
2022-01-29 12:04:44 +0200

    display refactoring

--- src/DisplayManager.cpp
+++ src/DisplayManager.cpp
@@ -1,29 +1,28 @@
 #include <DisplayManager.h>
 #include <Adafruit_SSD1306.h>
+
 #include <messaging/ParamsMessage.h>
 
 void DisplayManager::runTask(void *pvParam){
-  #define SDA 21
-  #define SCL 22
-  Wire.begin(SDA, SCL);
   Adafruit_SSD1306 display(128, 64, &Wire, 64);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C, 200000U)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     logger("SSD1306 allocation failed");
     for(;;);
   }
   display.display();
   delay(2000);
   display.clearDisplay();
+  display.stopscroll();
+  display.setTextSize(2);             
+  display.setTextColor(SSD1306_WHITE);
 
   ParamsMessage msg;
   while (true)
   {
     if (xQueueReceive((QueueHandle_t)pvParam, &msg, portMAX_DELAY) == pdTRUE)
     {
+      logger("got message");
       display.clearDisplay();
-      display.stopscroll();
-      display.setTextSize(2);             
-      display.setTextColor(SSD1306_WHITE);
       display.setCursor(0,0);
       display.printf("Current %i\n", msg.currentTemp);
       display.printf("Target  %i\n", msg.targetTemp);
@@ -34,5 +33,6 @@ void DisplayManager::runTask(void *pvParam){
       }
       display.display();
     }
+    logger("out of mesage");
   }
 }
