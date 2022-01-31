76c94186f81113e2d7622bbef6201498355d2d8c
ohumeniuk <ohumeniuk@crownresorts.com.au>
2022-01-31 16:37:57 +0200

    switched to async web server

--- src/HttpApiManageger.cpp
+++ src/HttpApiManageger.cpp
@@ -1,11 +1,13 @@
 #include <messaging/ParamsMessage.h>
 #include <Globals.h>
 #include <HttpApiManageger.h>
-#include <WebServer.h>
+#include <AsyncTCP.h>
+#include <AsyncJson.h>
+#include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
 
 ParamsMessage msg;
-WebServer server(80);
+AsyncWebServer server(80);
 
 void HttpApiManageger::runTask(void *pvParam){
   QueueHandle_t* queues = (QueueHandle_t *) pvParam;
@@ -17,7 +19,7 @@ void HttpApiManageger::runTask(void *pvParam){
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 
-  server.on("/getCurentState", HTTP_GET, [](){
+  server.on("/getCurentState", HTTP_GET, [](AsyncWebServerRequest *request){
     StaticJsonDocument<100> doc;
     JsonObject obj = doc.to<JsonObject>();
     obj["target"] = msg.targetTemp;
@@ -26,28 +28,24 @@ void HttpApiManageger::runTask(void *pvParam){
 
     String response;
     serializeJson(doc, response);
-    server.send(200, "application/json", response);
+    request->send(200, "application/json", response);
   });
 
-  server.on("/postSettings", HTTP_POST, [input](){
-    StaticJsonDocument<100> doc;
-    String json = server.arg("plain");
-    deserializeJson(doc, json);
-
-    JsonObject object = doc.as<JsonObject>();
+  AsyncCallbackJsonWebHandler* handler = new AsyncCallbackJsonWebHandler("/postSettings", [input](AsyncWebServerRequest *request, JsonVariant &json) {
+    JsonObject object = json.as<JsonObject>();
     msg.power = object["power"];
     msg.targetTemp = object["target"];
     msg.command = TT_POWER_SET;
     xQueueSend(input, &msg, portMAX_DELAY);
 
-    server.send(200);
+    request->send(200);
   });
 
+  server.addHandler(handler);
   server.begin();
 
 
   while (true){
-    server.handleClient();
     xQueueReceive(http, &msg, portMAX_DELAY);
    }
 }
