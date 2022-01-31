0db796df78b5965b58f47e204fe0207626bc4519
ohumeniuk <ohumeniuk@crownresorts.com.au>
2022-01-31 16:01:37 +0200

    refactoring

--- src/HttpApiManageger.cpp
+++ src/HttpApiManageger.cpp
@@ -6,48 +6,46 @@
 
 ParamsMessage msg;
 WebServer server(80);
-QueueHandle_t input;
-QueueHandle_t http;
-
-void getCurrentSettings(){
-  StaticJsonDocument<100> doc;
-  JsonObject obj = doc.to<JsonObject>();
-  obj["target"] = msg.targetTemp;
-  obj["current"] = msg.currentTemp;
-  obj["power"] = msg.power;
-
-  String response;
-  serializeJson(doc, response);
-  server.send(200, "application/json", response);
-}
-
-void postSettings(){
-  StaticJsonDocument<100> doc;
-  String json = server.arg("plain");
-  deserializeJson(doc, json);
-
-  JsonObject object = doc.as<JsonObject>();
-  msg.power = object["power"];
-  msg.targetTemp = object["target"];
-  msg.command = TT_POWER_SET;
-  xQueueSend(input, &msg, portMAX_DELAY);
-
-  server.send(200);
-}
 
 void HttpApiManageger::runTask(void *pvParam){
   QueueHandle_t* queues = (QueueHandle_t *) pvParam;
-  input = queues[0];
-  http = queues[1];
+  QueueHandle_t input = queues[0];
+  QueueHandle_t http = queues[1];
   free(pvParam);
 
   while (WiFi.status() != WL_CONNECTED) {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 
-  server.on("/getCurentState", HTTP_GET, getCurrentSettings);
-  server.on("/postSettings", HTTP_POST, postSettings);
+  server.on("/getCurentState", HTTP_GET, [](){
+    StaticJsonDocument<100> doc;
+    JsonObject obj = doc.to<JsonObject>();
+    obj["target"] = msg.targetTemp;
+    obj["current"] = msg.currentTemp;
+    obj["power"] = msg.power;
+
+    String response;
+    serializeJson(doc, response);
+    server.send(200, "application/json", response);
+  });
+
+  server.on("/postSettings", HTTP_POST, [input](){
+    StaticJsonDocument<100> doc;
+    String json = server.arg("plain");
+    deserializeJson(doc, json);
+
+    JsonObject object = doc.as<JsonObject>();
+    msg.power = object["power"];
+    msg.targetTemp = object["target"];
+    msg.command = TT_POWER_SET;
+    xQueueSend(input, &msg, portMAX_DELAY);
+
+    server.send(200);
+  });
+
   server.begin();
+
+
   while (true){
     server.handleClient();
     xQueueReceive(http, &msg, portMAX_DELAY);
